48f81192fe53e5a0da71b27a5a3d087330d16922
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-06 20:33:36 +0200

    Custom User Model

--- Areas/Identity/Data/DatabaseTest5User.cs
+++ Areas/Identity/Data/DatabaseTest5User.cs
@@ -9,5 +9,8 @@ namespace DatabaseTest5.Areas.Identity.Data
     // Add profile data for application users by adding properties to the DatabaseTest5User class
     public class DatabaseTest5User : IdentityUser
     {
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public string Function { get; set; }
     }
 }
