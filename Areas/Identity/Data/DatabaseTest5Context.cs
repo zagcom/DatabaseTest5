f52380dd9e9afdb29172ffa71face11212b62f74
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-06 12:22:38 +0200

    Adding Identity to project

---
+++ Areas/Identity/Data/DatabaseTest5Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatabaseTest5.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseTest5.Models
+{
+    public class DatabaseTest5Context : IdentityDbContext<DatabaseTest5User>
+    {
+        public DatabaseTest5Context(DbContextOptions<DatabaseTest5Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DatabaseTest5.Areas.Identity.Data;
+using DatabaseTest5.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DatabaseTest5.Areas.Identity.IdentityHostingStartup))]
+namespace DatabaseTest5.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DatabaseTest5Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DatabaseTest5ContextConnection")));
+
+                services.AddDefaultIdentity<DatabaseTest5User>()
+                    .AddEntityFrameworkStores<DatabaseTest5Context>();
+            });
+        }
+    }
+}
