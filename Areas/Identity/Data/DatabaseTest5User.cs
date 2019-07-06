6aa24c1e77c7464d5f28a9e7b81706d91f1bfb24
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-06 22:53:55 +0200

    Add [PersonalData] in custom user model

--- Areas/Identity/Data/DatabaseTest5User.cs
+++ Areas/Identity/Data/DatabaseTest5User.cs
@@ -9,8 +9,11 @@ namespace DatabaseTest5.Areas.Identity.Data
     // Add profile data for application users by adding properties to the DatabaseTest5User class
     public class DatabaseTest5User : IdentityUser
     {
+        [PersonalData]
         public string Name { get; set; }
+        [PersonalData]
         public string Unit { get; set; }
+        [PersonalData]
         public string Function { get; set; }
     }
 }
