44afb5ced209d34db5a7455ea828e80ee130f784
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-07 20:43:45 +0200

    Creating RoleList

---
+++ TagHelpers/RoleUserTH.cs
@@ -0,0 +1,41 @@
+﻿using DatabaseTest5.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseTest5.TagHelpers
+{
+    [HtmlTargetElement("td", Attributes = "i-role")]
+    public class RoleUsersTH : TagHelper
+    {
+        private UserManager<DatabaseTest5User> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public RoleUsersTH(UserManager<DatabaseTest5User> usermgr, RoleManager<IdentityRole> rolemgr)
+        {
+            userManager = usermgr;
+            roleManager = rolemgr;
+        }
+
+        [HtmlAttributeName("i-role")]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+            IdentityRole role = await roleManager.FindByIdAsync(Role);
+            if (role != null)
+            {
+                foreach (var user in userManager.Users)
+                {
+                    if (user != null && await userManager.IsInRoleAsync(user, role.Name))
+                        names.Add(user.UserName);
+                }
+            }
+            output.Content.SetContent(names.Count == 0 ? "No Users" : string.Join(", ", names));
+        }
+    }
+}
