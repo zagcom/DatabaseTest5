283efe16da803a132e8fa7dd33051148ca369322
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-08 19:16:46 +0200

    Comment Create Role section in RoleController

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -22,38 +22,38 @@ namespace Identity.Controllers
 
         public ViewResult Index() => View(roleManager.Roles);
 
-        public IActionResult Create() => View();
+        //public IActionResult Create() => View();
 
-        [HttpPost]
-        public async Task<IActionResult> Create([Required]string name)
-        {
-            if (ModelState.IsValid)
-            {
-                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
-                if (result.Succeeded)
-                    return RedirectToAction("Index");
-                else
-                    Errors(result);
-            }
-            return View(name);
-        }
+        //[HttpPost]
+        //public async Task<IActionResult> Create([Required]string name)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+        //        if (result.Succeeded)
+        //            return RedirectToAction("Index");
+        //        else
+        //            Errors(result);
+        //    }
+        //    return View(name);
+        //}
 
-        [HttpPost]
-        public async Task<IActionResult> Delete(string id)
-        {
-            IdentityRole role = await roleManager.FindByIdAsync(id);
-            if (role != null)
-            {
-                IdentityResult result = await roleManager.DeleteAsync(role);
-                if (result.Succeeded)
-                    return RedirectToAction("Index");
-                else
-                    Errors(result);
-            }
-            else
-                ModelState.AddModelError("", "No role found");
-            return View("Index", roleManager.Roles);
-        }
+        //[HttpPost]
+        //public async Task<IActionResult> Delete(string id)
+        //{
+        //    IdentityRole role = await roleManager.FindByIdAsync(id);
+        //    if (role != null)
+        //    {
+        //        IdentityResult result = await roleManager.DeleteAsync(role);
+        //        if (result.Succeeded)
+        //            return RedirectToAction("Index");
+        //        else
+        //            Errors(result);
+        //    }
+        //    else
+        //        ModelState.AddModelError("", "No role found");
+        //    return View("Index", roleManager.Roles);
+        //}
 
         public async Task<IActionResult> Update(string id)
         {
