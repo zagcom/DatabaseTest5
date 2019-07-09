495de33e06bf6dad3dbd1310bcba8ab1d928f693
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-09 19:19:21 +0200

    Creating list of users and their roles

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatabaseTest5.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using DatabaseTest5.Models;
+
+namespace DatabaseTest5.Controllers
+{
+    public class UserController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        private UserManager<DatabaseTest5User> userManager;
+        public UserController(UserManager<DatabaseTest5User> userMrg, RoleManager<IdentityRole> roleMgr)
+        {
+            roleManager = roleMgr;
+            userManager = userMrg;
+        }
+
+        public ViewResult Index() => View(userManager.Users);
+
+    }
+}
+
+
+
---
+++ TagHelpers/UserRoleTH.cs
@@ -0,0 +1,40 @@
+﻿using DatabaseTest5.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseTest5.TagHelpers
+{
+    [HtmlTargetElement("td", Attributes = "i-user")]
+    public class UserRolesTH : TagHelper
+    {
+        private UserManager<DatabaseTest5User> userManager;
+        private RoleManager<IdentityRole> roleManager;
+        public UserRolesTH(UserManager<DatabaseTest5User> usermgr, RoleManager<IdentityRole> rolemgr)
+        {
+            userManager = usermgr;
+            roleManager = rolemgr;
+        }
+
+        [HtmlAttributeName("i-user")]
+        public string User { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+            DatabaseTest5User user = await userManager.FindByIdAsync(User);
+            if (user != null)
+            {
+                foreach (var role in roleManager.Roles)
+                {
+                    if (role != null && await userManager.IsInRoleAsync(user, role.Name))
+                        names.Add(role.Name);
+                }
+            }
+            output.Content.SetContent(names.Count == 0 ? "No Roles" : string.Join(", ", names));
+        }
+    }
+}
