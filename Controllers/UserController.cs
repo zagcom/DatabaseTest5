529fcbbf46c79f4f31557722ac1aa460ee8ae1ba
Maciej Zagrajek <maciej.zagrajek@gmail.com>
2019-07-09 20:05:25 +0200

    Add and remove users to roles

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,9 +6,11 @@ using DatabaseTest5.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using DatabaseTest5.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DatabaseTest5.Controllers
 {
+    [Authorize(Roles = "MasterUser")]
     public class UserController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
@@ -20,6 +22,64 @@ namespace DatabaseTest5.Controllers
         }
 
         public ViewResult Index() => View(userManager.Users);
+
+        public async Task<IActionResult> Update(string id)
+        {
+            DatabaseTest5User user = await userManager.FindByIdAsync(id);
+            List<IdentityRole> members = new List<IdentityRole>();
+            List<IdentityRole> nonMembers = new List<IdentityRole>();
+            foreach (IdentityRole role in roleManager.Roles)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(role);
+            }
+            return View(new UserEdit
+            {
+                User = user,
+                UserRoles = members,
+                UserNonRoles = nonMembers
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UserModification model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string roleName in model.RoleAdds ?? new string[] { })
+                {
+                    DatabaseTest5User user = await userManager.FindByIdAsync(model.UserId);
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, roleName);
+                        if (!result.Succeeded)
+                            Errors(result);
+                    }
+                }
+                foreach (string roleName in model.RoleDels ?? new string[] { })
+                {
+                    DatabaseTest5User user = await userManager.FindByIdAsync(model.UserId);
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, roleName);
+                        if (!result.Succeeded)
+                            Errors(result);
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+                return RedirectToAction(nameof(Index));
+            else
+                return await Update(model.UserId);
+        }
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
 
     }
 }
---
+++ Models/UserEdit .cs
@@ -0,0 +1,16 @@
+﻿using DatabaseTest5.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseTest5.Models
+{
+    public class UserEdit
+    {
+        public DatabaseTest5User User { get; set; }
+        public IEnumerable<IdentityRole> UserRoles { get; set; }
+        public IEnumerable<IdentityRole> UserNonRoles { get; set; }
+    }
+}
